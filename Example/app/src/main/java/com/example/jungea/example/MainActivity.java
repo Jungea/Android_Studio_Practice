e6e0f848bc5643047ba63bd69bd90924f818399d
Jungea <jungea2241@gmail.com>
2019-03-14 20:29:09 +0900

    버튼 클릭 Toast 메세지

--- Example/app/src/main/java/com/example/jungea/example/MainActivity.java
+++ Example/app/src/main/java/com/example/jungea/example/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jungea.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "시작버튼이 눌렸어요", Toast.LENGTH_LONG).show();
+        //잠깐 보였다가 없어지는 메세지 표시
     }
 }
